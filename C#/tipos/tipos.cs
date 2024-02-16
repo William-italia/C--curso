abd1e554f35433f19ee370f4747bd81fd723e4a1
Chart 70 <williamitalia70@outlook.com>
2024-02-16 13:58:40 -0300

    c# course

---
+++ C#/tipos/tipos.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+float n5 = 4.5F;
+
+string nome = "William";
+object obj1 = "Joao";
+object obj2 = 4.5F;
+
+int n1 = int.MinValue;
+int n1M = int.MaxValue;
+
+double saldo = 10.35784;
+
+System.Console.WriteLine($"{saldo:f2}");
+System.Console.WriteLine(saldo.ToString("F2"));
+System.Console.WriteLine(saldo.ToString("F2", CultureInfo.InvariantCulture));
+System.Console.WriteLine(n5);
+System.Console.WriteLine(n1);
+System.Console.WriteLine(n1M);
+System.Console.WriteLine(nome);
+System.Console.WriteLine(obj1);
+System.Console.WriteLine(obj2);
